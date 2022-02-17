4f779a83b9b00b3a511176ff82d0c7ab0856dbea
AkashiCoin <55268546+AkashiCoin@users.noreply.github.com>
2022-02-17 15:26:01 +0800

    Update bt_in.sh

--- bt_in.sh
+++ bt_in.sh
@@ -1,3 +1,9 @@
 wget -O install.sh http://download.bt.cn/install/install_6.0.sh && echo y | bash install.sh
+wget http://download.bt.cn/install/update/LinuxPanel-7.7.0.zip \
+&& unzip LinuxPanel-7.7.0.zip \
+&& cd panel \
+&& bash update.sh \
+&& cd .. && rm -f LinuxPanel-7.7.0.zip && rm -rf panel
 rm -f /www/server/panel/data/admin_path.pl
+sed -i "s|bind_user == 'True'|bind_user == 'Sakura'|" /www/server/panel/BTPanel/static/js/index.js
 /usr/sbin/sshd -D
