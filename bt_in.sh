67997b749441b7a05ee3f828aaeace3ffe8ad222
AkashiCoin <55268546+AkashiCoin@users.noreply.github.com>
2022-02-17 17:53:05 +0800

    Update bt_in.sh

--- bt_in.sh
+++ bt_in.sh
@@ -1,3 +1,3 @@
 wget -O install.sh http://download.bt.cn/install/install_6.0.sh && echo y | bash install.sh
 rm -f /www/server/panel/data/admin_path.pl 
-sed -i "s|bind_user == 'True'|bind_user == 'Sakura'|" /www/server/panel/BTPanel/static/js/index.js
+/usr/sbin/sshd -D
